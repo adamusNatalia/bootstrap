5bdabd1de5263b481b38dd9f3c2fc3d85bb2bf54
Student 212832 <212832@student.pwr.edu.pl>
2018-09-19 17:17:25 +0200

    Almased

---
+++ bootstrapProject/js/almased.js
@@ -0,0 +1,7 @@
+$(function () {
+    $(document).scroll(function () {
+        var winH = $(window).height()/3;
+        var $nav = $("#mainNav");
+        $nav.toggleClass('navbar-shrink', $(this).scrollTop() >  winH);
+    });
+});
