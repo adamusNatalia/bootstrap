d981e5696b7f97d4b6a14e89860c939fa5793655
Student 212832 <212832@student.pwr.edu.pl>
2018-10-12 13:33:44 +0200

    dieta

--- bootstrapProject/js/almased.js
+++ bootstrapProject/js/almased.js
@@ -1,7 +1,12 @@
-$(function () {
+(function ($) {
+
+
     $(document).scroll(function () {
         var winH = $(window).height()/3;
         var $nav = $("#mainNav");
         $nav.toggleClass('navbar-shrink', $(this).scrollTop() >  winH);
     });
-});
+
+
+
+})(jQuery);
