32ddce4b6d550e07e17c2e6f12d6ca082f79d078
Student 212832 <212832@student.pwr.edu.pl>
2018-10-19 16:12:00 +0200

    footer

--- bootstrapProject/js/almased.js
+++ bootstrapProject/js/almased.js
@@ -1,6 +1,11 @@
 (function ($) {
 
 
+
+    $(window).load(function() {
+        $(".loader").fadeOut("slow");
+    });
+
     $(document).scroll(function () {
         var winH = $(window).height()/3;
         var $nav = $("#mainNav");
